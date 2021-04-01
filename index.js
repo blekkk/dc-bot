788959dbf0997398c1dc96cb1aaf53741309e31d
blekkk <fazailman992@gmail.com>
2021-04-01 12:01:00 +0700

    Added tugas functions
    Add, list, delete tugas to/from database

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const pool = new Pool({
     ssl: {
         rejectUnauthorized: false
     }
-  });
+});
 
 const handleSafeBooru = (client, booruParams) => {
 
@@ -54,6 +54,90 @@ const handleNotSafeBooru = (client, booruParams) => {
     }
 }
 
+const handleAddTugas = (client, params) => {
+    let queryArr = [];
+    params.replace(/(?<=")\s+(?=")/g, '').match(/[^\".+\"]+/g).forEach((elem) => {
+        queryArr.push(elem.replace(/"/g, "'"))
+    });
+    if (queryArr.length !== 3) {
+        client.channel.send('Wrong input!');
+        return;
+    }
+    pool.query(`INSERT INTO public."List_Tugas"(mata_kuliah,detail_tugas,deadline_tugas)
+  VALUES('${queryArr[0]}','${queryArr[1]}','${queryArr[2]}')`, (err, res) => {
+        if (err) throw err;
+        client.react('✅');
+    });
+}
+
+const handleListTugas = (client, params) => {
+    if (params.length !== 0) {
+        client.channel.send('No params needed!');
+        return;
+    }
+    let finalText = "";
+    finalText = finalText.concat(`List Tugas:\n\n`);
+    pool.query(`SELECT * FROM public."List_Tugas"
+  ORDER BY id ASC `, (err, res) => {
+        let i = 1;
+        if (err || res.rows.length === 0) {
+            console.log(err);
+            client.channel.send('Tidak ada tugas');
+            return;
+        }
+        for (let row of res.rows) {
+            finalText = finalText.concat(`**No ${i}.**\n**Nama MK**: ${row.mata_kuliah}\n**Detail Tugas**: ${row.detail_tugas}\n**Deadline Tugas**: ${row.deadline_tugas}\n\n`);
+            i++;
+        }
+        client.channel.send(finalText);
+    });
+}
+
+const deleteTugasCheck = (client, queryParams) => {
+    pool.query(`SELECT * FROM public."List_Tugas"
+  ORDER BY id ASC `, (err, res) => {
+        if (err) throw err;
+        if (res.rows.length === 0) {
+            client.channel.send('Tidak ada tugas untuk didelete');
+            return;
+        }
+        if (queryParams > res.rows.length) {
+            client.react('❎');
+            return;
+        } else {
+            client.react('✅');
+        }
+    });
+}
+
+const handleDeleteTugas = (client, params) => {
+    let queryParams = 0;
+    queryParams = parseInt(params);
+    if (Number.isInteger(queryParams) === false) {
+        client.channel.send('Wrong input!');
+        return;
+    }
+    deleteTugasCheck(client, queryParams);
+    pool.query(`do $$
+  <<delete_bangsat>>
+  declare
+      row_pointer integer;
+  begin
+      SELECT id into row_pointer
+      FROM (
+        SELECT
+          ROW_NUMBER() OVER (ORDER BY id ASC) AS rownumber,
+          id
+        FROM public."List_Tugas"
+      ) AS foo
+      WHERE rownumber = ${queryParams};
+      DELETE FROM public."List_Tugas"
+      WHERE id = row_pointer;
+  end delete_bangsat $$;`, (err, res) => {
+        if (err) throw err;
+    });
+}
+
 const handleJadwalKuliah = (client, params) => {
     let dayToday = "null";
     let dayOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri'];
@@ -68,13 +152,10 @@ const handleJadwalKuliah = (client, params) => {
     }
     if (params.match(/^mon|tue|wed|thu|fri$/g) && dayToday === "null") {
       params = params.replace(/"/g, "'");
-      console.log(dayToday);
       finalText = finalText.concat(`Jadwal kuliah hari **${params}**:\n\n`);
       pool.query(`SELECT nama_kuliah,hari_kuliah,dosen,waktu_kuliah FROM public."Jadwal_Kuliah" WHERE hari_kuliah = '${params}' ORDER BY id ASC `, (err, res) => {
         if (err) throw err;
         for (let row of res.rows) {
-          console.log(finalText);
-          console.log(row);
           finalText = finalText.concat(`**Nama MK**: ${row.nama_kuliah}\n**Dosen**: ${row.dosen}\n**Waktu Kuliah**: ${row.waktu_kuliah}\n\n`);
         }
         client.channel.send(finalText);
@@ -82,13 +163,10 @@ const handleJadwalKuliah = (client, params) => {
     } else if (dayToday !== "null" && params.length === 0) {
       dayToday = dayOfWeek[dayToday - 1];
       dayToday = dayToday.replace(/"/g, "'");
-      console.log(dayToday);
       finalText = finalText.concat(`Jadwal kuliah hari ini(**${dayToday}**):\n\n`);
       pool.query(`SELECT nama_kuliah,hari_kuliah,dosen,waktu_kuliah FROM public."Jadwal_Kuliah" WHERE hari_kuliah = '${dayToday}' ORDER BY id ASC `, (err, res) => {
         if (err) throw err;
         for (let row of res.rows) {
-          console.log(finalText);
-          console.log(row);
           finalText = finalText.concat(`**Nama MK**: ${row.nama_kuliah}\n**Dosen**: ${row.dosen}\n**Waktu Kuliah**: ${row.waktu_kuliah}\n\n`);
         }
         client.channel.send(finalText);
@@ -97,7 +175,7 @@ const handleJadwalKuliah = (client, params) => {
       client.channel.send("Please input the correct day (mon - fri)");
       return;
     }
-  }
+}
 
 const handleNhentaiInfo = async (client, params) => {
     try {
@@ -292,12 +370,14 @@ const handleHelpReact = (client, message) => {
         client.reply('nhentai-info sends you information about the doujin with the given id\n\nExample: blek! nhentai-info 177013\n\nNote: nhentai-info only accepts **one** id');
     } else if (message[2] === 'jadwal') {
         client.reply('jadwal is a hidden function that sends you information about today\'s coolyeah schedule or a chosen day (mon-fri)\n\nExample:\n\`blek! jadwal\` (for today\'s schedule)\n\`blek! jadwal mon\` (for schedule on monday)');
+    } else if (message[2] === 'tugas') {
+        client.reply('the hidden tugas family functions, use it to add, list, and delete your tugas. Here\'s how to use it:\n\nAdd your tugas:\n\`blek! tugas-add "<MATA_KULIAH>" "<DETAIL_TUGAS>" "<DEADLINE_TUGAS>"\`\nList your tugas:\n\`blek! tugas-list\`\nDelete your tugas:\n\`blek! tugas-delete <NOMOR_TUGAS_PADA_LIST>\`');
     }
 }
 
 client.on('message', msg => {
-
     var cleanMsg = msg.content.toLowerCase().split(' ');
+    var tugasMsg = msg.content.split(' ');
     var booruParams = [];
     for (let i = 2; i < cleanMsg.length; i++) {
         booruParams.push(cleanMsg[i]);
@@ -313,6 +393,20 @@ client.on('message', msg => {
         }
         if (cleanMsg[1] === 'jadwal') {
             handleJadwalKuliah(msg, booruParams);
+        }
+        if (cleanMsg[1] === 'tugas-add') {
+            let tugasParams = [];
+            for (let i = 2; i < cleanMsg.length; i++) {
+                tugasParams.push(tugasMsg[i]);
+            }
+            tugasParams = tugasParams.join(' ');
+            handleAddTugas(msg, tugasParams);
+        }
+        if (cleanMsg[1] === 'tugas-list') {
+            handleListTugas(msg, booruParams);
+        }
+        if (cleanMsg[1] === 'tugas-delete') {
+            handleDeleteTugas(msg, booruParams);
         }
         if (cleanMsg[1] === '-h') {
             handleHelpReact(msg, cleanMsg);
